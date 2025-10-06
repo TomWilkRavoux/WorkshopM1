5675b0d8ab979bfd4afcf230f057e8a85ee84390
Snowgou <tomwilkwilk@gmail.com>
2025-10-06 15:51:06 +0200

    Archi clean + strat dev

---
+++ frontend/escape-tech/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-3xl text-red-900">
+          Coucou
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
