d005699d3e6c891e03af629901e543e93b582920
Marc6Alex <marcalex.nezout@ynov.com>
2025-10-08 11:46:16 +0200

    Update App.tsx

--- frontend/escape-tech/src/App.tsx
+++ frontend/escape-tech/src/App.tsx
@@ -5,6 +5,7 @@ import Lobby from './components/Lobby'
 import Home from './components/Home'
 import MedecinPage from './components/Medecin'
 import PharmacienPage from './components/Pharmacien'
+import Endgame from './components/EndGame'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/room" element={<RoomTest />} />
         <Route path="/medecin" element={<PharmacienPage />} />
         <Route path="/pharmacien" element={<MedecinPage />} />
+        <Route path="/endgame" element={<Endgame />} />
       </Routes>
     </Router>
   )
